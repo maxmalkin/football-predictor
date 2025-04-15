fbd3606e222d2bfac157f401db647554e7019915
Max Malkin <maxim_malkin@outlook.com>
2025-04-15 11:53:53 -0600

    CORS config

--- packages/server/internal/api/routes.go
+++ packages/server/internal/api/routes.go
@@ -1,11 +1,34 @@
 package api
 
 import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
-	r := gin.Default()
-	r.GET("/fixtures", GetFixtures)
-	return r
+	router := gin.Default()
+
+	clientHost := os.Getenv("CLIENT_HOST")
+	clientPort := os.Getenv("CLIENT_PORT")
+
+	allowedOrigin := fmt.Sprintf("%s:%s", clientHost, clientPort)
+
+	// CORS config
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{allowedOrigin},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	// routes
+	router.POST("/fixtures", GetFixtures)
+
+	return router
 }
