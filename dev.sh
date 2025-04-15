f134bd95e3f80d74621510fe9a00874919f783c3
Max Malkin <maxim_malkin@outlook.com>
2025-04-14 21:24:55 -0600

    watch scripts for dev servers

--- dev.sh
+++ dev.sh
@@ -4,5 +4,5 @@ concurrently \
   -n "client,server,model" \
   -c "cyan,green,yellow" \
   "cd packages/client && bun run dev" \
-  "cd packages/server && go run main.go" \
-  "cd packages/model && cargo run"
+  "cd packages/server && reflex -c reflex.conf" \
+  "cd packages/model && cargo watch -x run"
