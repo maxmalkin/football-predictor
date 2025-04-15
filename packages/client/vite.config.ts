ea4f76df6e1ad25e71ed9d4c279e67037615731c
Max Malkin <maxim_malkin@outlook.com>
2025-04-15 12:24:08 -0600

    downgrade tailwind from alpha

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import dotenv from "dotenv";
 dotenv.config({ path: "../../.env" });
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [react()],
 	server: {
 		port: Number(process.env.CLIENT_PORT) || 5173,
 	},
